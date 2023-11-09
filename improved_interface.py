2716f96fe240134dd553d0d8f04c3b318dbf0198
timur <timurka.zavyalov@mail.ru>
2023-11-09 22:59:39 +0300

    add: added docker logs

--- improved_interface.py
+++ improved_interface.py
@@ -1,3 +1,4 @@
+import psutil
 import docker
 import telebot
 import requests
@@ -7,6 +8,7 @@ from datetime import datetime
 from docker import DockerClient
 from telebot.types import Message
 from dadata.sync import DadataClient
+from psutil._common import bytes2human
 from requests import exceptions, Response
 
 
@@ -22,20 +24,26 @@ def start_menu(message: Message, is_back: bool):
 
     markup: types.InlineKeyboardMarkup = types.InlineKeyboardMarkup()
     button_check_db: types.InlineKeyboardButton = \
-        types.InlineKeyboardButton(text='Подключение к базе данных', callback_data='check_db')
+        types.InlineKeyboardButton(text='🛢️ Подключение к базе данных', callback_data='check_db')
     button_check_yandex: types.InlineKeyboardButton = \
-        types.InlineKeyboardButton(text='Баланс на Яндекс.Кошельке', callback_data='check_yandex')
+        types.InlineKeyboardButton(text='💰 Баланс на Яндекс.Кошельке', callback_data='check_yandex')
     button_check_dadata: types.InlineKeyboardButton = \
-        types.InlineKeyboardButton(text='Оставшиеся количество запросов в Dadata', callback_data='check_dadata')
+        types.InlineKeyboardButton(text='📊 Оставшиеся количество запросов в Dadata', callback_data='check_dadata')
     button_get_logs_docker: types.InlineKeyboardButton = \
-        types.InlineKeyboardButton(text='Логи контейнеров', callback_data='get_logs_docker')
+        types.InlineKeyboardButton(text='🐳 Логи контейнеров', callback_data='get_logs_docker')
+    button_get_ram_memory: types.InlineKeyboardButton = \
+        types.InlineKeyboardButton(text='🖥️ Оперативная память', callback_data='get_ram_memory')
+    button_get_rom_memory: types.InlineKeyboardButton = \
+        types.InlineKeyboardButton(text='💾 Внутренняя память', callback_data='get_rom_memory')
     button_get_chat_id: types.InlineKeyboardButton = \
-        types.InlineKeyboardButton(text='Chat ID', callback_data='get_chat_id')
+        types.InlineKeyboardButton(text='🆔 Chat ID', callback_data='get_chat_id')
 
     markup.row(button_check_db)
     markup.row(button_check_yandex)
     markup.row(button_check_dadata)
     markup.row(button_get_logs_docker)
+    markup.row(button_get_ram_memory)
+    markup.row(button_get_rom_memory)
     markup.row(button_get_chat_id)
 
     if is_back:
@@ -100,7 +108,7 @@ def get_chat_id(message: Message) -> None:
 def get_logs_docker(message: Message) -> None:
     markup: types.InlineKeyboardMarkup = types.InlineKeyboardMarkup()
     buttons: list = [
-        types.InlineKeyboardButton(container, callback_data=f'get_log_container_{container}')
+        types.InlineKeyboardButton(f"🐳 {container}", callback_data=f'get_log_container_{container}')
         for container in sorted([container.name for container in client.containers.list()])
     ]
     for i in range(0, len(buttons), 2):
@@ -108,8 +116,8 @@ def get_logs_docker(message: Message) -> None:
             markup.add(buttons[i], buttons[i + 1])
         else:
             markup.add(buttons[i])
-    markup.add(types.InlineKeyboardButton('Вернуться в меню', callback_data='back'))
-    markup.add(types.InlineKeyboardButton('Закрыть', callback_data='close'))
+    markup.add(types.InlineKeyboardButton('⏪ Назад', callback_data='back'))
+    markup.add(types.InlineKeyboardButton('❌ Закрыть', callback_data='close'))
     try:
         bot.edit_message_text('Выберите контейнер для получения логов:', message.chat.id, message.message_id,
                               reply_markup=markup)
@@ -118,10 +126,22 @@ def get_logs_docker(message: Message) -> None:
                          reply_markup=markup)
 
 
+@bot.message_handler(commands=['get_ram_memory'])
+def get_ram_memory(message: Message) -> None:
+    bot.reply_to(message, f'Занято оперативной памяти(%):\n{psutil.virtual_memory().percent}\n\n'
+                          f'Занято оперативной памяти(GB):\n{bytes2human(psutil.virtual_memory().used)}')
+
+
+@bot.message_handler(commands=['get_rom_memory'])
+def get_rom_memory(message: Message) -> None:
+    bot.reply_to(message, f'Занято внутренней памяти(%):\n{psutil.disk_usage("/").percent}\n\n'
+                          f'Занято внутренней памяти(GB):\n{bytes2human(psutil.disk_usage("/").used)}')
+
+
 def get_log_container(message: Message, container_name: str) -> None:
     logs: bytes = client.containers.get(container_name).logs(tail=10)
     lines: list = logs.decode('utf-8').split("\n")
-    last_lines: str = "\n".join(lines)
+    last_lines: str = "\n\n".join(lines)
     bot.reply_to(message, f'Логи контейнера {container_name}:\n{last_lines}')
 
 
@@ -140,6 +160,8 @@ def callback_handler(call: types.CallbackQuery):
             'check_yandex': check_balance_xml_river,
             'check_dadata': check_num_requests_dadata,
             'get_logs_docker': get_logs_docker,
+            'get_ram_memory': get_ram_memory,
+            'get_rom_memory': get_rom_memory,
             'get_chat_id': get_chat_id
         }
         if call.data in data_actions:
